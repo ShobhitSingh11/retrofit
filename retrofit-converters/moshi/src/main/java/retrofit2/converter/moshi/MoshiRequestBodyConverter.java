f1c0169e288253e3f49c1d55d1fcf2627c27dd09
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-02-08 16:49:17 +0530

    Update MoshiRequestBodyConverter.java

--- retrofit-converters/moshi/src/main/java/retrofit2/converter/moshi/MoshiRequestBodyConverter.java
+++ retrofit-converters/moshi/src/main/java/retrofit2/converter/moshi/MoshiRequestBodyConverter.java
@@ -29,7 +29,7 @@ final class MoshiRequestBodyConverter<T> implements Converter<T, RequestBody> {
   private final JsonAdapter<T> adapter;
 
   MoshiRequestBodyConverter(JsonAdapter<T> adapter) {
-    // rfhbjergfjher4bgilrdefedfedefdfe
+    // rfhbjergfjher4bgilrdefedfedefdfedcedfevfvfrev
     this.adapter = adapter;
   }
 
